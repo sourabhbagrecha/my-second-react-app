8e8dfd3d21c41b67b8f9e7b4417e3a3c31bbe3f5
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2019-12-31 14:46:00 +0530

    Updates

---
+++ precache-manifest.c93c7dd91888d04fe9f131c2d0bed5e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48765677fb4c7e4223657bc3e1503ae",
+    "url": "/my-second-react-app/index.html"
+  },
+  {
+    "revision": "a69e14e50821c9a23eb6",
+    "url": "/my-second-react-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f1cb4b75d81e89c0e73f",
+    "url": "/my-second-react-app/static/css/main.fe1181ff.chunk.css"
+  },
+  {
+    "revision": "a69e14e50821c9a23eb6",
+    "url": "/my-second-react-app/static/js/2.d9fd406d.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/my-second-react-app/static/js/2.d9fd406d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1cb4b75d81e89c0e73f",
+    "url": "/my-second-react-app/static/js/main.49ae7ff4.chunk.js"
+  },
+  {
+    "revision": "3681312924d8c39dcd9b",
+    "url": "/my-second-react-app/static/js/runtime-main.c993be24.js"
+  }
+]);
